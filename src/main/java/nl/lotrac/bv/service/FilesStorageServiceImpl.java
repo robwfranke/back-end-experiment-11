7449ae8cf38be1d4dd42fc8c07f92db888e414de
=Rob Franke <robwfranke@gmail.com>
2021-05-27 15:00:56 +0200

    alle benodigde files erin, nu testen

---
+++ src/main/java/nl/lotrac/bv/service/FilesStorageServiceImpl.java
@@ -0,0 +1,96 @@
+package nl.lotrac.bv.service;
+
+
+import nl.lotrac.bv.model.FileEntity;
+import nl.lotrac.bv.repository.FileRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@Slf4j
+@Service
+public class FilesStorageServiceImpl implements FilesStorageService {
+
+
+  @Autowired
+  private FileRepository fileRepository;
+
+
+//    private FileRepository fileRepository;
+
+
+    private final Path root = Paths.get("uploads");
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectory(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
+    }
+
+    @Override
+    public void save(MultipartFile file) {
+
+        try {
+
+            FileEntity fileEntity = new FileEntity();
+
+
+            fileEntity.setName(StringUtils.cleanPath(file.getOriginalFilename()));
+            fileEntity.setContentType(file.getContentType());
+            fileEntity.setData(file.getBytes());
+            fileEntity.setSize(file.getSize());
+
+            fileRepository.save(fileEntity);
+
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+
+    @Override
+    public Stream<Path> loadAll() {
+        try {
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load the files!");
+        }
+    }
+
+}
